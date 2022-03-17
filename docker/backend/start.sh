9172f811c45f63699ac5247d556c3bcbdce03ec0
Jordi Mateo <jordi.mateo@udl.cat>
2022-03-17 22:25:24 +0000

    wait-for improved

--- docker/backend/start.sh
+++ docker/backend/start.sh
@@ -3,6 +3,8 @@
 pip install --upgrade pip
 pip install -r /app/requirements.txt
 
+/scripts/wait-for-it.sh $DAMCore_DB_HOST:3306
+
 export PYTHONPATH=$PYTHONPATH:/app
 python /app/dev/reset_database.py
 
