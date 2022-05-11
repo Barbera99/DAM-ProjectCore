a7f67912da04bd649d9ddac81149a63cfce66914
Barbera99 <josep.barbera.reverte.1999@gmail.com>
2022-05-11 11:30:16 +0100

    logs

--- resources/user_resources.py
+++ resources/user_resources.py
@@ -39,6 +39,9 @@ class ResourceRegisterUser(DAMCoreResource):
         super(ResourceRegisterUser, self).on_post(req, resp, *args, **kwargs)
         mylogger.info("Creant usuari...")
         user = User()
+        mylogger.info(req.media["username"])
+        mylogger.info(req.media["password"])
+        mylogger.info(req.media["email"])
         try:
             '''
             try:
